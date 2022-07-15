f338a76e1a32be6438e1e1cf44f4e5abb50785b7
Li Cheng <lilybearcheng@Lis-MacBook-Pro.home>
2022-07-15 10:33:22 -0700

    updated table schema

--- server/models/PostgreSQLModel.js
+++ server/models/PostgreSQLModel.js
@@ -19,20 +19,22 @@ module.exports = {
 
 /*
 CREATE TABLE appointment (
-  appt_id integer PRIMARY KEY,
+  appt_id BIGSERIAL PRIMARY KEY,
   doc_id integer NOT NULL,
   ptFirstName varchar NOT NULL,
   ptLastName varchar NOT NULL,
   date DATE NOT NULL,
-  time TIME NOT NULL check (MINUTE(time) = '00' or MINUTE(time) = '15' or MINUTE(time) = '30' or MINUTE(time) = '45'),
+  time TIME NOT NULL, 
   kind varchar NOT NULL check (kind = 'Follow-up' or kind = 'New Patient'),
-  created_at timestamp default CURRENT_TIMESTAMP,
+  created_at timestamp default CURRENT_TIMESTAMP
 );
 
+check (MINUTE(time) = '00' or MINUTE(time) = '15' or MINUTE(time) = '30' or MINUTE(time) = '45'),
+
 CREATE TABLE doctor (
-  doc_id integer PRIMARY KEY,
+  doc_id BIGSERIAL PRIMARY KEY,
   firstName varchar NOT NULL,
   lastNAME varchar NOT NULL,
 )
-
+INSERT INTO appointments (doc_id, ptFirstName, ptLastName, date, time, kind) VALUES (1, 'ABC', 'E', '2022-08-22', '08:30:00', 'follow-up')
 */
